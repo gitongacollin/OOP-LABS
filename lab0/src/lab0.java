369028ee501154c2ce6ad1c7b25e59c2df8ae701
gitongacollin <gitongacollin@users.noreply.github.com>
2017-12-06 18:33:17 +0300

    Add files via upload

---
+++ lab0/src/lab0.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class lab0 {
+
+    public class InnerClass{
+        public String printer(String text){
+            System.out.println(text);
+            return text;
+        }
+    
+    }
+    public static void main(String[] args) {
+        try{
+            String input = "is";
+        int user_input = Integer.parseInt(input);
+        }catch(Exception p){
+            System.out.println("An error has occured");
+        }
+        
+    }
+    
+}
